a04a317cc182bf1b32607708b7731d5c8dffd17b
BorimirGanchev <99996450+BorimirGanchev@users.noreply.github.com>
2023-10-23 10:13:13 +0300

    merge sort

---
+++ PractiseThree/mergeSort.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+void merge(int arr[], int left, int middle, int right) {
+    int n1 = middle - left + 1;
+    int n2 = right - middle;
+
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[middle + 1 + j];
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int middle = left + (right - left) / 2;
+
+        mergeSort(arr, left, middle);
+        mergeSort(arr, middle + 1, right);
+
+        merge(arr, left, middle, right);
+    }
+}
+
+int main() {
+    int arr[] = {12, 13, 4, 5, 16, 17};
+    int arrSize = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Unsorted array: ");
+    for (int i = 0; i < arrSize; i++) {
+        printf("%d, ", arr[i]);
+    }
+
+    mergeSort(arr, 0, arrSize - 1);
+
+    printf("\nSorted array: ");
+    for (int i = 0; i < arrSize; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
