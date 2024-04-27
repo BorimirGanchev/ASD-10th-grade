c4af6227bb21546568177533230d4b9a830c3328
BorimirGanchev <99996450+BorimirGanchev@users.noreply.github.com>
2024-04-27 17:02:34 +0300

    better

--- Homework4/travel.c
+++ Homework4/travel.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
 
@@ -18,6 +18,12 @@ typedef struct Graph {
     Node **nodes;
     int num_nodes;
 } Graph;
+
+typedef struct PathInfo {
+    Node **path;
+    int pathLength;
+    int totalWeight;
+} PathInfo;
 
 Node* createNode(char *value) {
     Node *newNode = (Node*)malloc(sizeof(Node));
@@ -49,28 +55,18 @@ void addEdge(Node *node1, Node *node2, int weight) {
     node1->neighbors[node1->num_neighbors++] = newEdge;
 }
 
-void printGraph(Graph *graph) {
-    printf("Map in Europe:\n");
-    for (int i = 0; i < graph->num_nodes; ++i) {
-        Node *node = graph->nodes[i];
-        printf(" %s -> ", node->value);
-        for (int j = 0; j < node->num_neighbors; ++j) {
-            Edge *edge = node->neighbors[j];
-            printf("%s(%d), ", edge->destination->value, edge->weight);
-        }
-        for (int j = 0; j < graph->num_nodes; ++j) {
-            Node *otherNode = graph->nodes[j];
-            for (int k = 0; k < otherNode->num_neighbors; ++k) {
-                Edge *edge = otherNode->neighbors[k];
-                if (edge->destination == node) {
-                    printf("%s(%d), ", otherNode->value, edge->weight);
-                }
-            }
-        }
-
-        printf("\n");
-    }
-}   
+// void printGraph(Graph *graph) {
+//     printf("Map in Europe:\n");
+//     for (int i = 0; i < graph->num_nodes; ++i) {
+//         Node *node = graph->nodes[i];
+//         printf(" %s -> ", node->value);
+//         for (int j = 0; j < node->num_neighbors; ++j) {
+//             Edge *edge = node->neighbors[j];
+//             printf("%s(%d), ", edge->destination->value, edge->weight);
+//         }
+//         printf("\n");
+//     }
+// }
 
 void freeGraph(Graph *graph) {
     for (int i = 0; i < graph->num_nodes; ++i) {
@@ -85,101 +81,73 @@ void freeGraph(Graph *graph) {
     free(graph);
 }
 
-bool BFS(Graph *graph, Node *start, Node *end, int maxSum, int maxStops) {
+void printPath(Node **path, int pathLength, int totalWeight) {
+    printf("Path: ");
+    for (int i = 0; i < pathLength; ++i) {
+        printf("%s", path[i]->value);
+        if (i < pathLength - 1)
+            printf(" -> ");
+    }
+    printf("\nTotal cost: %d\n", totalWeight);
+}
+
+int comparePathInfo(const void *a, const void *b) {
+    const PathInfo *path1 = (const PathInfo *)a;
+    const PathInfo *path2 = (const PathInfo *)b;
+    return path1->totalWeight - path2->totalWeight;
+}
+
+void DFS(Graph *graph, Node *start, Node *end, int maxSum, int maxStops, PathInfo *pathInfos, int *numPaths, Node **path, int pathLength, int currentWeight, int currentStops) {
     if (start == NULL || end == NULL || graph == NULL)
-        return false;
+        return;
 
-    bool *visited = (bool*)malloc(sizeof(bool) * graph->num_nodes);
-    memset(visited, false, sizeof(bool) * graph->num_nodes);
+    path[pathLength++] = start;
 
-    int *queue = (int*)malloc(sizeof(int) * graph->num_nodes);
-    int front = 0, rear = 0;
-    queue[rear++] = 0;
-    visited[0] = true;
+    if (start == end && currentWeight <= maxSum && currentStops <= maxStops) {
+        pathInfos[*numPaths].path = (Node **)malloc(pathLength * sizeof(Node *));
+        memcpy(pathInfos[*numPaths].path, path, pathLength * sizeof(Node *));
+        pathInfos[*numPaths].pathLength = pathLength;
+        pathInfos[*numPaths].totalWeight = currentWeight;
+        (*numPaths)++;
+    } else {
+        for (int i = 0; i < start->num_neighbors; ++i) {
+            Node *neighbor = start->neighbors[i]->destination;
+            int weight = start->neighbors[i]->weight;
 
-    int *parent = (int*)malloc(sizeof(int) * graph->num_nodes);
-    memset(parent, -1, sizeof(int) * graph->num_nodes);
-
-    int *weights = (int*)malloc(sizeof(int) * graph->num_nodes);
-    memset(weights, 0, sizeof(int) * graph->num_nodes);
-
-    while (front < rear) {
-        int currentNode = queue[front++];
-        Node *current = graph->nodes[currentNode];
-
-        if (current == end) {
-            int currentNodeIndex = currentNode;
-            int totalWeight = 0;
-            int totalStops = 0;
-            printf("Path: %s", end->value);
-            while (parent[currentNodeIndex] != -1) {
-                int parentNodeIndex = parent[currentNodeIndex];
-                Node *parentNode = graph->nodes[parentNodeIndex];
-                Edge *edge = NULL;
-                for (int i = 0; i < parentNode->num_neighbors; ++i) {
-                    if (parentNode->neighbors[i]->destination == graph->nodes[currentNodeIndex]) {
-                        edge = parentNode->neighbors[i];
-                        break;
-                    }
-                }
-                if (edge != NULL) {
-                    totalWeight += edge->weight;
-                    totalStops++; // Increment stops only when moving to a new node
-                }
-                printf(" <- %s", graph->nodes[parentNodeIndex]->value);
-                currentNodeIndex = parentNodeIndex;
-            }
-            printf("\nTotal Weight: %d\n", totalWeight);
-            // printf(" Total stops: %d\n", totalStops);
-            // printf(" Max stops: %d\n", maxStops);
-            if (totalWeight <= maxSum && totalStops-1 <= maxStops) {
-                free(visited);
-                free(queue);
-                free(parent);
-                free(weights);
-                return true;
-            } else {
-                free(visited);
-                free(queue);
-                free(parent);
-                free(weights);
-                return false;
-            }
-        }
-
-        for (int i = 0; i < current->num_neighbors; ++i) {
-            Node *neighbor = current->neighbors[i]->destination;
-            int neighborIndex = -1;
-            for (int j = 0; j < graph->num_nodes; ++j) {
-                if (graph->nodes[j] == neighbor) {
-                    neighborIndex = j;
+            bool alreadyVisited = false;
+            for (int j = 0; j < pathLength; ++j) {
+                if (path[j] == neighbor) {
+                    alreadyVisited = true;
                     break;
                 }
             }
-            if (neighborIndex != -1 && !visited[neighborIndex]) {
-                queue[rear++] = neighborIndex;
-                visited[neighborIndex] = true;
-                parent[neighborIndex] = currentNode;
+
+            if (!alreadyVisited) {
+                int newWeight = currentWeight + weight;
+                int newStops = currentStops + 1;
+                if (newWeight <= maxSum && newStops <= maxStops) {
+                    DFS(graph, neighbor, end, maxSum, maxStops, pathInfos, numPaths, path, pathLength, newWeight, newStops);
+                }
             }
         }
     }
-
-    free(visited);
-    free(queue);
-    free(parent);
-    free(weights);
-    return false;
 }
-
 
 int main() {
     Graph *graph = createGraph();
 
-    int maxSum = 900;
-    int maxStops = 2;
+    int maxSum = 0;
+    
+    while(maxSum <= 0 ){
+        printf("Enter the maximum sum: ");
+        scanf("%d", &maxSum);
+    }
 
-    //printf("Enter the max sum for the flight: ");
-    //scanf("%d", &maxSum);
+    int maxStops = 0;
+    while(maxStops <= 0 ){
+        printf("Enter the maximum tops: ");
+        scanf("%d", &maxStops);
+    }
 
     addNode(graph, "Madrid");//0
     addNode(graph, "Paris");//1
@@ -188,22 +156,39 @@ int main() {
     addNode(graph, "Sofia");//4
     addNode(graph, "Berlin");//5
 
-    addEdge(graph->nodes[0], graph->nodes[1], 100);//Madrid Paris 100
-    addEdge(graph->nodes[1], graph->nodes[2], 200);//Paris London 200
-    addEdge(graph->nodes[0], graph->nodes[3], 300);//Madrid Rome 300
-    addEdge(graph->nodes[3], graph->nodes[4], 400);//Rome Sofia 400
-    addEdge(graph->nodes[4], graph->nodes[5], 500);//Sofia Berlin  500
-    addEdge(graph->nodes[2], graph->nodes[5], 600);//London Berlin 600
+    // addEdge(graph->nodes[0], graph->nodes[1], 100);
+    // addEdge(graph->nodes[1], graph->nodes[2], 200);
+    // addEdge(graph->nodes[0], graph->nodes[3], 300);
+    // addEdge(graph->nodes[3], graph->nodes[4], 400);
+    // addEdge(graph->nodes[4], graph->nodes[5], 500);
+    // addEdge(graph->nodes[2], graph->nodes[5], 600);
 
-    printGraph(graph);
+    addEdge(graph->nodes[0], graph->nodes[1], 100);
+    addEdge(graph->nodes[0], graph->nodes[3], 100);
+    addEdge(graph->nodes[1], graph->nodes[2], 100);
+    addEdge(graph->nodes[2], graph->nodes[3], 100);
+    addEdge(graph->nodes[3], graph->nodes[5], 100);
+    addEdge(graph->nodes[3], graph->nodes[4], 100);
 
-    if (BFS(graph, graph->nodes[0], graph->nodes[5], maxSum, maxStops)) {
-        printf("There is a path between Madrid and Berlin.\n");
-    } else {
-        printf("There is no path between Madrid and Berlin.\n");
+    //printGraph(graph);
+
+    printf("Here are all paths from the most cheapest to the most expensive:\n");
+
+    PathInfo *pathInfos = (PathInfo *)malloc(graph->num_nodes * sizeof(PathInfo));
+    int numPaths = 0;
+    Node **path = (Node **)malloc(graph->num_nodes * sizeof(Node *));
+    DFS(graph, graph->nodes[0], graph->nodes[5], maxSum, maxStops+1, pathInfos, &numPaths, path, 0, 0, 0);
+
+    qsort(pathInfos, numPaths, sizeof(PathInfo), comparePathInfo);
+
+    for (int i = 0; i < numPaths; ++i) {
+        printPath(pathInfos[i].path, pathInfos[i].pathLength, pathInfos[i].totalWeight);
+        free(pathInfos[i].path);
     }
 
     freeGraph(graph);
+    free(path);
+    free(pathInfos);
 
     return 0;
 }
